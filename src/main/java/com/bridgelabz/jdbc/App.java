d0a510c9d4413dfa216f631f9239f4a4dbb1dbee
s-h-u-b-s07 <shubhamgawali.sag@gmail.com>
2021-12-27 20:38:17 +0530

    ADDED: update query

--- src/main/java/com/bridgelabz/jdbc/App.java
+++ src/main/java/com/bridgelabz/jdbc/App.java
@@ -32,18 +32,20 @@ public class App
 //            String date = sc.next();
 
             //4 Execute query
-            String query = "select * from employee_payroll;";
-           // int result = stmt.executeUpdate(query);
-            ResultSet result = stmt.executeQuery(query);
-            while (result.next()) {
-                String id = result.getString(1);
-                String name = result.getString(2);
-                String gender = result.getString(3);
-                String salary = result.getString(4);
-                String startdate = result.getString(5);
-                System.out.println("ID : "+ id + ", Name : "+ name + ", Gender : "+ gender + ", Salary : "+salary+
-                        ", StartDate : " + startdate);
-            }
+//            String query = "select * from employee_payroll;";
+            String query = "update employee_payroll set salary='30000' where name = 'Shubham';";
+            int result = stmt.executeUpdate(query);
+//            ResultSet result = stmt.executeQuery(query);
+//            while (result.next()) {
+//                String id = result.getString(1);
+//                String name = result.getString(2);
+//                String gender = result.getString(3);
+//                String salary = result.getString(4);
+//                String startdate = result.getString(5);
+//                System.out.println("ID : "+ id + ", Name : "+ name + ", Gender : "+ gender + ", Salary : "+salary+
+//                        ", StartDate : " + startdate);
+//            }
+
         } catch (SQLException e) {
             e.printStackTrace();
         }finally {
